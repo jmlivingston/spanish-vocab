63558dd477da250c186667dbec7c230be837c5d1
John Livingston <jmlivingston@gmail.com>
2023-12-17 17:51:23 -0800

    test

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/spanish-vocab/",
+  base: "/<REPO>/",
   plugins: [
     VitePWA({
       includeAssets: ["vite.svg"],
