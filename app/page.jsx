373575860e1a1b86fe58f001a02d13fa5c50c4ef
John Livingston <jmlivingston@gmail.com>
2023-12-19 10:39:05 -0800

    test

--- app/page.jsx
+++ app/page.jsx
@@ -9,6 +9,7 @@ export const dynamic = "force-dynamic";
 export default function Home() {
   return (
     <main className='relative flex min-h-screen flex-col items-center justify-center'>
+      <h1>test</h1>
       <Link
         href='https://vercel.com/templates/next.js/kv-redis-starter'
         className='group mt-20 sm:mt-0 rounded-full flex space-x-1 bg-white/30 shadow-sm ring-1 ring-gray-900/5 text-gray-600 text-sm font-medium px-10 py-2 hover:shadow-lg active:shadow-sm transition-all'
